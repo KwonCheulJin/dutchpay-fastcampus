38d936e17e1af3470ae6b43747cca86f44d94f4d
KwonCheulJin <chkftm12@gmail.com>
2023-01-23 16:58:34 +0900

    그룹 생성 컴포넌트 UI 테스트 코드 및 테스팅 환경 셋업

---
+++ src/components/CreateGroup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { CenteredOverlayForm } from './CenteredOverlayForm'
+
+export const CreateGroup = () => {
+  return (
+    <div>Create Group component
+
+      <CenteredOverlayForm />
+    </div>
+  )
+}
